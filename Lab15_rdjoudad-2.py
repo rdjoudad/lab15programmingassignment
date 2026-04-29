e5dc3b614b1eecf66e3aec80e9e4ddfec11c1d88
Ryma Djoudad <zenyod85@gmail.com>
2026-04-29 19:44:32 -0400

    Changed Lab option name

---
+++ Lab15_rdjoudad-2.py
@@ -0,0 +1,12 @@
+"""
+Visualizing Data 
+Ryma Djoudad
+Make a graph representing a math formula using matplotlib
+No starter code
+4/29/2026
+"""
+
+import matplotlib as plt 
+import numpy as np
+
+
