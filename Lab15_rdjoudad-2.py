5ab20af24dada46b32b15a227a0f748c826a512c
Ryma Djoudad <zenyod85@gmail.com>
2026-04-29 20:07:57 -0400

    Generated graph image

--- Lab15_rdjoudad-2.py
+++ Lab15_rdjoudad-2.py
@@ -13,11 +13,12 @@ output = [1, 8, 27, 64, 125]
 
 my_fig, my_plot = plt.subplots()
 
-my_plot.plot(input, output, linewidth=2)
+my_plot.plot(input, output, linewidth=2, color='red')
 my_plot.set_title("First 5 Cubes", fontsize=30)
 my_plot.set_xlabel("Value", fontsize=20)
 my_plot.set_ylabel("Cube of Value", fontsize=20)
 
+my_fig.savefig('cubes_5.png')
 plt.show()
 
 
