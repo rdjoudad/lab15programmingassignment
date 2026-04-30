7885be125189acc971490b0bd6c79a5a7d5735e1
Ryma Djoudad <zenyod85@gmail.com>
2026-04-29 20:04:30 -0400

    First successful graph

--- Lab15_rdjoudad-2.py
+++ Lab15_rdjoudad-2.py
@@ -6,7 +6,19 @@ No starter code
 4/29/2026
 """
 
-import matplotlib as plt 
-import numpy as np
+import matplotlib.pyplot as plt 
+
+input = [1, 2, 3, 4, 5]
+output = [1, 8, 27, 64, 125]
+
+my_fig, my_plot = plt.subplots()
+
+my_plot.plot(input, output, linewidth=2)
+my_plot.set_title("First 5 Cubes", fontsize=30)
+my_plot.set_xlabel("Value", fontsize=20)
+my_plot.set_ylabel("Cube of Value", fontsize=20)
+
+plt.show()
+
 
 
